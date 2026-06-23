1f5b021314f4d41e75ce5427cb8807cba7a96f40
Lia Mongili <lmongili@c1r3p7.42firenze.it>
2026-06-23 18:00:47 +0200

    baseModel usage

--- parser.py
+++ parser.py
@@ -1,6 +1,8 @@
 from graph import Graph, Zone, Connection
 from typing import List, Dict
 from enum import Enum
+from pydantic import BaseModel, Field, model_validator
+from typing_extensions import Self
 
 
 class ParsingTags(Enum):
@@ -8,7 +10,64 @@ class ParsingTags(Enum):
     END_HUB = "end_hub"
     HUB = "hub"
     CONNECTION = "connection"
-    TAGS = [START_HUB, END_HUB, HUB, CONNECTION]
+
+
+class ParsingZones(Enum):
+    START = "start"
+    END = "end"
+    NORMAL = "normal"
+    RESTRICTED = "restricted"
+    PRIORITY = "priority"
+    BLOCKED = "blocked"
+
+
+class ParsingColors(Enum):
+    RED = "#FF0000"
+    GREEN = "#00FF00"
+    BLUE = "#0000FF"
+    YELLOW = "#FFFF00"
+    WHITE = "#FFFFFF"
+    PINK = "#E994D7"
+
+
+class MetaData(BaseModel):
+    type_data: str = Field(min_length=3)
+    zone: ParsingZones | None = None
+    color: ParsingColors = ParsingColors.WHITE
+    max_drones: int | None = Field(ge=0, default=None)
+    max_link_capacity: int | None = Field(ge=0, default=None)
+
+    @model_validator(mode="after")
+    def validator(self) -> Self:
+        self.type_data = self.type_data.lower()
+        if self.type_data not in ["hub", "connection"]:
+            raise ValueError("Expecting type_data to be "
+                             "either 'hub' or 'connection'")
+        elif self.type_data == "hub" and self.max_link_capacity:
+            raise ValueError("'hub' type_data expects "
+                             "field 'max_link_capacity' to be None")
+        elif self.type_data == 'connection' and self.zone or self.max_drones:
+            raise ValueError("'connection' type_data expects "
+                             "fields 'max_drones' and 'zone' to be None")
+        
+        return self
+
+
+class HubData(BaseModel):
+    tag: ParsingTags
+    name: str = Field(min_length=1)
+    x: int = Field(ge=0)
+    y: int = Field(ge=0)
+    metadata: MetaData | None
+
+    @model_validator(mode="after")
+    def validator(self) -> Self:
+        return self
+
+
+class MapData(BaseModel):
+    nb_drones: int = Field(ge=0)
+    hubs: List[HubData] = Field(min_length=2)
 
 
 class ParsingException(Exception):
@@ -89,7 +148,7 @@ class Parser:
         return int(nb_drones)
 
     @staticmethod
-    def parse_zone(row: str) -> Dict[str, str]:
+    def __parse_zone(row: str) -> Dict[str, str]:
         tags: List[str] = ["start_hub", "end_hub", "hub", "connection"]
 
         zone_dict: Dict[str, str] = {}
@@ -97,35 +156,56 @@ class Parser:
         metadata: List[str]
         tag: str
         row = row.strip()
-        try:
-            if len(row.split('[')) >= 1:
-                zone_data = row.split("[")[0].split(' ')
-                zone_data = [d for d in zone_data if d.replace(' ', '')]
-                if len(row.split('[')) == 2:
-                    Parser.__parse_metadata(zone_data[1].split("]")[0])
-            else:
-                raise ParsingException()
+        if len(row.split('[')) >= 1:
+            zone_data = row.split("[")[0].split(' ')
+            zone_data = [d for d in zone_data if d.replace(' ', '')]
+            if len(row.split('[')) == 2:
+                Parser.__parse_metadata(zone_data[1].split("]")[0])
+        else:
+            raise ParsingException()
 
-            if len(row.split(':')) != 2:
-                raise ParsingException
-            if row.split(':')[0].strip() not in tags:
-                raise ParsingException
-
-        except ParsingException as e:
-            raise e
+        if len(row.split(':')) != 2:
+            raise ParsingException
+        if row.split(':')[0].strip() not in tags:
+            raise ParsingException
+        
         return zone_dict
 
     @staticmethod
     def __parse_metadata(row: str) -> Dict[str, str]:
-        tags: tuple = ("zone", "color", "max_drones")
         meta_dict: Dict[str, str] = {}
         metadata: List[str] = row.split(' ')
-        metadata = [d for d in metadata
-                    if d.strip().startswith(tags)]
-        
+        key: str
+        value: str
+
+        for i in range(len(metadata)):
+            tmp = metadata[i]
+            if not tmp.strip():
+                metadata.pop(i)
+            elif not tmp.strip().startswith(tags):
+                raise ParsingException()
+            else:
+                metadata[i] = tmp.strip()
+
+        for d in metadata:
+            if len(d.split('=')) == 2:
+                key, value = d.split('=')
+                value = value.strip()
+            else:
+                raise ParsingException()
+            if key == "zone" and value not in t_zone:
+                raise ParsingException()
+            elif key == "color" and value not in colors:
+                raise ParsingException()
+            elif key == "max_drones" and not value.isdigit():
+                raise ParsingException()
+            if not meta_dict[key]:
+                meta_dict[key] = value
+            else:
+                raise ParsingException()
         return meta_dict
 
 
 if __name__ == "__main__":
-    b = True if '  '.replace(' ', '') else False
+    b = True if "    ".strip() else False
     print(b)
